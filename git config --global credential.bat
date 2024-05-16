be669d5853c120ffb95bbe3dc16d912fae85ff0a
susloalejo <suslo.alejandro@gmail.com>
2024-05-16 11:12:05 -0500

    teoria

--- git config --global credential.bat
+++ git config --global credential.bat
@@ -30,3 +30,10 @@ create and merge commit
 squas and merge
 rebase and merge
 
+
+Cuando haces git rebase:
+
+los commits locales se eliminan de la rama temporalmente.
+se ejecuta un git pull.
+los commits locales se insertan nuevamente.
+Esto quiere decir que todos tus commits locales aparecen al final, después de los commits remotos. Esto es, si haces git log, los commits de la rama que has rebasado aparecen como si fueran más antiguos, independientemente de cuándo se hicieran.
