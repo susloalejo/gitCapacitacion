16d030fb6072c7587e55fe7fc31dd39e58c7f1c7
susloalejo <suslo.alejandro@gmail.com>
2024-05-16 10:36:56 -0500

    comentarios capacitacion

---
+++ git config --global credential.bat
@@ -0,0 +1,32 @@
+git config --global credential.helper wincred
+git config --global user.name "[Your Name]"
+git config --global user.email [YourEmail]
+git clone --depth=1 https://github.com...........
+
+stage changes (no permite subir cuando se hace commit y/ push)
+
+PS C:\codigoFuente\gitCapacitacion> git config --global credential.helper wincred
+PS C:\codigoFuente\gitCapacitacion> git config --global user.name "susloalejo"
+PS C:\codigoFuente\gitCapacitacion> git config --global user.email suslo.alejandro@gmail.com
+PS C:\codigoFuente\gitCapacitacion> git branch
+* master
+PS C:\codigoFuente\gitCapacitacion> 
+
+git fetch (prune) escanea ramas en repositorio
+
+crear repositorio y publicar
+git init 
+git add
+
+
+
+extensiones:
+
+git history
+
+strategies:
+
+create and merge commit 
+squas and merge
+rebase and merge
+
